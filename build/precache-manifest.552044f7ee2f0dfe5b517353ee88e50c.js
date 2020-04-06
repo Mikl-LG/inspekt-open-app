ca366c4f492c71d251fcff1b243ac2c9bc318d30
Mikl-LG <legallicmickael@inspekt.fr>
2020-04-06 13:08:34 +0200

    resolving bugs in formscatalog and nature files

---
+++ build/precache-manifest.552044f7ee2f0dfe5b517353ee88e50c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe3545ece0b7b8404801c1d3135196c1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1655d33a9782de94bfc3",
+    "url": "/static/css/2.2ce93ba3.chunk.css"
+  },
+  {
+    "revision": "ea90bf478af3296e13e6",
+    "url": "/static/css/main.b3177683.chunk.css"
+  },
+  {
+    "revision": "1655d33a9782de94bfc3",
+    "url": "/static/js/2.95d14a21.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/static/js/2.95d14a21.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea90bf478af3296e13e6",
+    "url": "/static/js/main.6b266200.chunk.js"
+  },
+  {
+    "revision": "0031e3849957be554dd2",
+    "url": "/static/js/runtime-main.34594f18.js"
+  },
+  {
+    "revision": "1cedb6e919bfed6a2c1ec00b5d8ee620",
+    "url": "/static/media/UploadIcon.1cedb6e9.svg"
+  },
+  {
+    "revision": "8e49c94621a728b70e5f4fa436da03c9",
+    "url": "/static/media/logoRakord600.8e49c946.jpg"
+  }
+]);
